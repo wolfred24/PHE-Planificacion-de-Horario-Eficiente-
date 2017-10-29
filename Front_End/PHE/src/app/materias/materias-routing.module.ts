4f7e10529adddc577a6e74e68eb8c8c2058b741e
isaacTrujillo <isaac_filos2000@outlook.com>
2017-10-28 19:45:35 -0500

    Se modico el componente materias para que muestre las materias actuales

--- Front_End/PHE/src/app/materias/materias-routing.module.ts
+++ Front_End/PHE/src/app/materias/materias-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { MateriasComponent } from './materias.component';
+import { AgregarMateriaComponent } from './agregar-materia/agregar-materia.component';
 
 const materiasRutas: Routes = [
-  {path: 'materias', component: MateriasComponent }
+  {path: 'materias', component: MateriasComponent },
+  {path: 'agregarMateria', component: AgregarMateriaComponent }
 ];
 
 @NgModule({
