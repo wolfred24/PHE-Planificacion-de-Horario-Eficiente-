d7db4a697307bf3be8629c510c6483f542071e6f
isaacTrujillo <isaac_filos2000@outlook.com>
2017-10-28 19:46:41 -0500

    se modico el componente principal y el modulo principal para aceptar los cambios

--- Front_End/PHE/src/app/materias/materias.module.ts
+++ Front_End/PHE/src/app/materias/materias.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MateriasRoutingModule } from './materias-routing.module';
+import { FormsModule } from '@angular/forms';
 
 import { MateriasComponent } from './materias.component';
+import { AgregarMateriaComponent } from './agregar-materia/agregar-materia.component';
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     MateriasRoutingModule
   ],
   declarations: [
-    MateriasComponent
+    MateriasComponent,
+    AgregarMateriaComponent
   ]
 })
 export class MateriasModule { }
